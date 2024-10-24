43f82baf27ba2e5e892423bfce3c4fb7b040573d
GITSangWoo <dogcenta@gmail.com>
2024-10-24 15:28:12 +0900

    first commit

---
+++ src/main/java/com/websocket/socket/controller/GreetingController.java
@@ -0,0 +1,19 @@
+package com.websocket.socket.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+import com.websocket.socket.greeting.Greeting;
+import com.websocket.socket.message.HelloMessage;
+
+@Controller
+public class GreetingController {
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting(message.getName()+":"+ HtmlUtils.htmlEscape(message.getMessage()));
+    }
+}
