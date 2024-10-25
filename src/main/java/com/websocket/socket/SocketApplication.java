0440d18d85651b1f992e1d2276d2b5a9729c3fc8
DONGUK777 <kimdongyork@gmail.com>
2024-10-25 15:34:47 +0900

    bug fix

--- src/main/java/com/websocket/socket/SocketApplication.java
+++ src/main/java/com/websocket/socket/SocketApplication.java
@@ -3,7 +3,7 @@ package com.websocket.socket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.websocket.service"})
 public class SocketApplication {
 
 	public static void main(String[] args) {
