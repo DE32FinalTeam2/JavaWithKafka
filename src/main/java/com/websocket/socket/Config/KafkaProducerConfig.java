b1f0988e6906137415a5728fbd1a1649a849aca7
GITSangWoo <dogcenta@gmail.com>
2024-10-27 11:07:55 +0900

    system with docker compl

--- src/main/java/com/websocket/socket/Config/KafkaProducerConfig.java
+++ src/main/java/com/websocket/socket/Config/KafkaProducerConfig.java
@@ -13,6 +13,7 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
+
 @Configuration
 @EnableKafka
 public class KafkaProducerConfig {
